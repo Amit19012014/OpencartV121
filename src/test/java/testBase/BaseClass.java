a1c8df4b73d1e958c7e3d1858524cf1d1037048c
Amit19012014 <Amit19012014@gmail.com>
2025-06-07 23:16:06 +0530

    07-June_2025

--- src/test/java/testBase/BaseClass.java
+++ src/test/java/testBase/BaseClass.java
@@ -1,26 +1,59 @@
 package testBase;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
 import org.apache.commons.lang3.RandomStringUtils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
-import java.time.Duration;
+import org.apache.logging.log4j.LogManager;//log4j
+import org.apache.logging.log4j.Logger;   //log4j
+import org.testng.annotations.Parameters;
+
 
 public class BaseClass {
+
     public WebDriver driver;
+    public Logger logger;
+    public Properties p;
+
 
 
     @BeforeClass
-    public void setup() {
-        driver = new ChromeDriver();
+    @Parameters({"os","browser"})
+    public void setup(String os ,String br) throws IOException {
+
+        //loading properties file
+        FileReader file=new FileReader(".\\src\\test\\resources\\config.proprties");
+        p=new Properties();
+        p.load(file);
+
+
+
+        logger = LogManager.getLogger(this.getClass());//Log4j
+
+        switch (br.toLowerCase())
+        {
+            case "chrome" : driver = new ChromeDriver(); break;
+            case "edge" : driver = new EdgeDriver(); break;
+            case "firefox" : driver = new FirefoxDriver(); break;
+            default:
+                System.out.println("invalid browser name"); return;
+        }
+
         driver.manage().deleteAllCookies();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        driver.get("https://tutorialsninja.com/demo/");
+        driver.get(p.getProperty("appURL"));
         driver.manage().window().maximize();
-
     }
 
     @AfterClass
@@ -32,18 +65,17 @@ public class BaseClass {
     public String randomeString() {
         String generatedString = RandomStringUtils.randomAlphabetic(5);
         return generatedString;
-
     }
 
-    public String numberString() {
-        String generatedNumber = RandomStringUtils.randomNumeric(10);
-        return generatedNumber;
+    public String randomeNumber() {
+        String generatedString = RandomStringUtils.randomNumeric(10);
+        return generatedString;
     }
 
     public String randomAlphaNumeric() {
-        String generatedString = RandomStringUtils.randomAlphabetic(5);
-        String generatedNumber = RandomStringUtils.randomNumeric(10);
-        return (generatedString + "@" + generatedString);
-    }
+        String str = RandomStringUtils.randomAlphabetic(3);
+        String num = RandomStringUtils.randomNumeric(3);
 
+        return (str + "@" + num);
+    }
 }
